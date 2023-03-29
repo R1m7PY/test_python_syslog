ad7f7107454cca82cac8911542ff61411fc4a4cf
R1m7PY <bibik.hg.15@gmail.com>
2023-03-29 11:03:38 +0500

    test5

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import time
 
 def Job():
     # syslog.openlog(ident="PythonTest", logoption=syslog.LOG_INFO, facility=syslog.LOG_AUTH)
-    syslog.syslog(syslog.LOG_INFO, "I started working")
+    syslog.syslog(syslog.LOG_DEBUG, "I started working")
     for i in range(5):
         time.sleep(2)
-        syslog.syslog(syslog.LOG_INFO, "I'm working")
-    syslog.syslog(syslog.LOG_INFO, "I'm done working")
+        syslog.syslog(syslog.LOG_DEBUG, "I'm working")
+    syslog.syslog(syslog.LOG_DEBUG, "I'm done working")
 
 
 if __name__ == '__main__':
