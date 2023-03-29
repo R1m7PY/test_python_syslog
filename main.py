bac4a89e2d6fb1fd8f77e75fdc9e78706badcf79
R1m7PY <bibik.hg.15@gmail.com>
2023-03-29 11:20:08 +0500

    test5

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-import syslog
 import time
+import logging
 
 
 def Job():
-    # syslog.openlog(ident="PythonTest", logoption=syslog.LOG_INFO, facility=syslog.LOG_AUTH)
-    syslog.syslog(syslog.LOG_DEBUG, "I started working")
+    # Настроить логгер
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # Создать обработчик файлового журнала и настроить уровень журнала
+    log_file = '/var/log/python_test.log'
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+
+    # Создать форматировщик и добавить его в обработчик
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    # Добавить обработчик к логгеру
+    logger.addHandler(file_handler)
+
+    logger.info("I started working")
     for i in range(5):
         time.sleep(2)
-        syslog.syslog(syslog.LOG_DEBUG, "I'm working")
-    syslog.syslog(syslog.LOG_DEBUG, "I'm done working")
+        logger.info("I'm working")
+    logger.info("I'm done working")
 
 
 if __name__ == '__main__':
