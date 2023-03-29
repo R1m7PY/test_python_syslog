f18e8d06bf5a85bc78dd816f3a79a49e102cadf7
R1m7PY <bibik.hg.15@gmail.com>
2023-03-29 08:25:36 +0500

    test1

---
+++ main.py
@@ -0,0 +1,15 @@
+import syslog
+import time
+
+
+def Job():
+    syslog.openlog(ident="python_test", logoption=syslog.LOG_INFO, facility=syslog.LOG_LOCAL1)
+    syslog.syslog("I started working")
+    for i in range(5):
+        time.sleep(2)
+        syslog.syslog("I'm working")
+    syslog.syslog("I'm done working")
+
+
+if __name__ == '__main__':
+    Job()
